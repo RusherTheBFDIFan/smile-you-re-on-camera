f9a5f145fea62727a39ac475fa8d2b01d7babc92
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2021-12-26 20:31:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -13,11 +14,25 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+    if (status != ""){
+        objectDetector.detect(video, gotResult);
+    }
 }
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects...";
+    for(i = 0; i < objects.length; i++){
+        document.getElementById("status").innerHTML =  "Status: Objects Detected!";
+        document.getElementById("numberofobjects").innerHTML = "Number of objects detected are: " + objects.length;
+
+        fill("#007FFF");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y+15);
+        noFill();
+        stroke("#007FFF");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
 }
 
 function modelLoaded(){
@@ -26,4 +41,12 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
